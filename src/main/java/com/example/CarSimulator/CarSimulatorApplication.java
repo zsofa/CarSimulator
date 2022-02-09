d70f4fe63c6454cabad234c1f01cf14b8a8812ca
zsofa <hzsofi12@gmail.com>
2022-02-09 11:18:23 +0100

    Initial commit

---
+++ src/main/java/com/example/CarSimulator/CarSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.CarSimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CarSimulator/CarSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CarSimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
